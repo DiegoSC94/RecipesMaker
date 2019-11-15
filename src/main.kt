241ae23753ccf37f637fd870dfdc5a0a2786c3f8
Diego Santamarta Castillo <disancas94@gmail.com>
2019-11-15 15:40:14 +0100

    Reto 2
    En este reto he modularizado la creación de la receta y la vista de las mismas con un par de funciones, makeRecipes y viewRecipes

--- src/main.kt
+++ src/main.kt
@@ -1,21 +1,16 @@
 
-fun main (args:Array<String>){
+//Listas
+val listaIngredientes:List<String> = listOf<String>("Agua", "Leche", "Carne", "Verduras", "Frutas", "Cereal", "Huevos", "Aceite")
+var listaRecetas:HashMap<String, List<String>> = hashMapOf()
 
-    //Listas
-    val listaIngredientes:List<String> = listOf<String>("Agua", "Leche", "Carne", "Verduras", "Frutas", "Cereal", "Huevos", "Aceite")
-    var listaRecetas:HashMap<String, List<String>> = hashMapOf()
-
-    //Variables
-    var nombreReceta:String
-    var seguir:String = "N"
-    var ingrediente:String
-
-    val menu:String = """
+val menu:String = """
         Selecciona la opción deseada
         1. Hacer una receta
         2. Ver mis recetas
         3. Salir
     """.trimIndent()
+
+fun main (args:Array<String>){
 
     //Empieza el programa
 
@@ -29,61 +24,12 @@ fun main (args:Array<String>){
 
         when(opcion){
             "1" -> {
-                do {
-                    println("Escribe el nombre para la receta")
-                    nombreReceta = readLine()?:"DSC"
-
-                    if (nombreReceta.isNullOrEmpty()) {
-                        println("No has escrito nada, escribe un nombre para la receta")
-                    }
-                } while (nombreReceta.isNullOrEmpty())
-
-                println("El nombre de la receta es $nombreReceta")
-
-                var ingredientesReceta:List<String> = listOf<String>()
-
-                do{
-                    do {
-                        println("Elige los ingredientes:\n")
-                        var num = 1
-                        for(ingredientes in listaIngredientes){
-                            println("$num. $ingredientes")
-                            num++
-                        }
-                        
-                        ingrediente = readLine()?:"DSC"
-
-                        if (ingrediente.isNotEmpty()) {
-
-                            ingredientesReceta += listaIngredientes.get(ingrediente.toInt() - 1)
-
-                            println("El ingrediente ${listaIngredientes.get(ingrediente.toInt() - 1)} se ha añadido a la lista")
-                            println("La lista contiene $ingredientesReceta")
-                            println("¿Deseas añadir otro ingrediente? S o N")
-
-                            seguir = readLine()?:"DSC"
-
-                        } else {
-                            println("El ingrediente añadido no esta en la lista")
-                        }
-                    } while (seguir.equals("S") || seguir.equals("s"))
-
-                    listaRecetas.put(nombreReceta, ingredientesReceta)
-
-                } while (ingredientesReceta.isNullOrEmpty() && ingrediente.isNullOrEmpty())
+                makeRecipe()
 
             }
 
             "2" -> {
-
-                if (listaRecetas.isNullOrEmpty()){
-                    println("No hay recetas en la lista\n")
-                }else {
-                    for(auxReceta in listaRecetas){
-                        println("Nombre de la receta: ${auxReceta.key}")
-                        println("Ingredientes: ${auxReceta.value} \n")
-                    }
-                }
+                viewRecipe()
             }
 
             "3" -> println("Saliendo del Programa")
@@ -92,5 +38,69 @@ fun main (args:Array<String>){
 
     }while (!opcion.equals("3"))
 
+}
+
+fun makeRecipe(){
+
+    //Variables
+    var nombreReceta:String
+    var seguir:String = "N"
+    var ingrediente:String
+
+    //Proceso
+    do {
+        println("Escribe el nombre para la receta")
+        nombreReceta = readLine()?:"DSC"
+
+        if (nombreReceta.isNullOrEmpty()) {
+            println("No has escrito nada, escribe un nombre para la receta")
+        }
+    } while (nombreReceta.isNullOrEmpty())
+
+    println("El nombre de la receta es $nombreReceta")
+
+    var ingredientesReceta:List<String> = listOf<String>()
+
+    do{
+        do {
+            println("Elige los ingredientes:\n")
+            var num = 1
+            for(ingredientes in listaIngredientes){
+                println("$num. $ingredientes")
+                num++
+            }
+
+            ingrediente = readLine()?:"DSC"
+
+            if (ingrediente.isNotEmpty()) {
+
+                ingredientesReceta += listaIngredientes.get(ingrediente.toInt() - 1)
+
+                println("El ingrediente ${listaIngredientes.get(ingrediente.toInt() - 1)} se ha añadido a la lista")
+                println("La lista contiene $ingredientesReceta")
+                println("¿Deseas añadir otro ingrediente? S o N")
+
+                seguir = readLine()?:"DSC"
+
+            } else {
+                println("El ingrediente añadido no esta en la lista")
+            }
+        } while (seguir.equals("S") || seguir.equals("s"))
+
+        listaRecetas.put(nombreReceta, ingredientesReceta)
+
+    } while (ingredientesReceta.isNullOrEmpty() && ingrediente.isNullOrEmpty())
+}
+
+fun viewRecipe(){
+
+    if (listaRecetas.isNullOrEmpty()){
+        println("No hay recetas en la lista\n")
+    }else {
+        for(auxReceta in listaRecetas){
+            println("Nombre de la receta: ${auxReceta.key}")
+            println("Ingredientes: ${auxReceta.value} \n")
+        }
+    }
 }
 
